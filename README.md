9c28c99fa439f9ab4f14d0ea91f721379bd95ce8
btwei <ben.stwei@gmail.com>
2024-08-14 17:16:33 -0500

    Added Diffuse and Specular Lighting

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# CPU-Raytracer
-This is a CPU implementation of ray tracing in Python
+# IN PROGRESS CPU-Raytracer
+This project showcases a CPU implementation of a raytracer in Python. It implements basic raytracing and a few extra bells and whistles. To try this yourself, clone the repository and run a test scene or construct your own.
+
+## Table of Contents
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Examples](#examples)
+- [Contact](#contact)
+
+## Features
+- Basic Raytracing: 
+- Primatives: 
+- Light & Shadows: 
+
+## Installation
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/btwei/CPU-Rasterizer.git
+    ```
+2. Navigate to the project directory:
+    ```bash
+    cd CPU-Rasterizer
+    ```
+3. Install dependencies:
+    ```bash
+    make install
+    ```
+
+## Usage
+To run the application,
+
+## Examples
+
+## Contact
+Ben Wei - [ben.stwei@gmail.com](mailto:ben.stwei@gmail.com)
+
+Project Link: [https://github.com/btwei/CPU-Raytracer](https://github.com/btwei/CPU-Raytracer)
--- Raytracer.py
+++ Raytracer.py
@@ -3,26 +3,46 @@ import numpy as np
 from PIL import Image
 
 ## Default Settings
-image_width = 250
-image_height = 250
+image_width = 1920
+image_height = 1080
 output_file = 'output.png'
 
 e = np.array([0.0, 0.0, 0.0])
 d = 1.0
-viewport_width = 1
-viewport_height = 1
+viewport_width = 1.3333333
+viewport_height = 0.75
 
-BACKGROUND_COLOR = (0,0,0,255)
+BACKGROUND_COLOR = np.array([0,0,0,255])
 
 ## Object Types
 
 class Sphere:
-    def __init__(self, center, radius, color):
+    def __init__(self, center, radius, color, specularity):
         self.center = center
         self.radius = radius
         self.color = color
+        self.specularity = specularity
 
 spheres = []
+
+class AmbientLight():
+    def __init__(self, intensity):
+        self.type = "ambient"
+        self.intensity = intensity
+
+class PointLight():
+    def __init__(self, intensity, position):
+        self.type = "point"
+        self.intensity = intensity
+        self.position = position
+
+class DirectionalLight():
+    def __init__(self, intensity, direction):
+        self.type = "ambient"
+        self.intensity = intensity
+        self.direction = direction
+
+lights = []
 
 ## Function Definitions
 def ScreenToViewport(x, y):
@@ -44,7 +64,15 @@ def TraceRay(origin, direction, t_min, t_max):
     if closest_object == None:
         return BACKGROUND_COLOR
     
-    return closest_object.color
+    point = e + closest_t * np.array(direction)
+    # Only works with spheres
+    normal = point - closest_object.center
+    normal = normal / (np.sqrt(normal.dot(normal)))
+
+    color = np.array(closest_object.color) * ComputeLighting(point, normal,-1*np.array(direction), closest_object.specularity)
+    color[3] = 255
+
+    return color
 
 def IntersectRaySphere(origin, direction, sphere):
     OC = origin - sphere.center
@@ -62,18 +90,53 @@ def IntersectRaySphere(origin, direction, sphere):
 
     return t1, t2
 
+def ComputeLighting(point, normal, view_angle, specularity):
+    intensity = 0.0
+    for light in lights:
+        if light.type == "ambient":
+            intensity += light.intensity
+        else:
+            if light.type == "point":
+                direction = light.position - point
+            else:
+                direction = light.direction
+            
+            # Diffuse Reflection
+            diffuse_dot = np.dot(normal, direction)
+            if diffuse_dot > 0:
+                intensity += light.intensity * diffuse_dot/(np.sqrt(normal.dot(normal)) * np.sqrt(direction.dot(direction)))
+
+            # Specular Reflection
+            if specularity != -1:
+                reflection = 2 * normal * np.dot(normal, direction) - direction
+                specular_dot = np.dot(reflection, view_angle)
+                if specular_dot > 0:
+                    intensity += light.intensity * np.pow(specular_dot/(np.sqrt(reflection.dot(reflection)) * np.sqrt(view_angle.dot(view_angle))), specularity)
+    
+    return intensity
+
 ## Main Code
 
 image = Image.new("RGBA", (image_width, image_height), (0,0,0,0))
 
-spheres.append(Sphere((0, 1, 3), 0.3, (60, 40, 10, 255)))
-spheres.append(Sphere((-2, 0.5, 2), 1, (10, 0, 30, 255)))
-spheres.append(Sphere((1, 2, 4.5), 2, (100, 0, 255, 255)))
+#Add Test Geometry
+spheres.append(Sphere((0, -1, 3), 1, (255, 0, 0, 255), 500))
+spheres.append(Sphere((2, 0, 4), 1, (0, 0, 255, 255), 500))
+spheres.append(Sphere((-2, 0, 4), 1, (0, 255, 0, 255), 10))
+spheres.append(Sphere((0, -5001, 0), 5000, (255, 255, 0, 255), 1000))
+
+#Add Test Light Sources
+lights.append(AmbientLight(0.2))
+lights.append(PointLight(0.6, (2, 1, 0)))
+lights.append(DirectionalLight(0.2, (1, 4, 4)))
 
 for x in range(image_width):
     for y in range(image_height):
         direction = ScreenToViewport(x, y)
+        # Color is a np array of floats from 0 to 255
         color = TraceRay(e, direction, 1, np.inf)
+        # Restore color to an integer and format as a tuple for pillow
+        color = tuple(np.round(color).astype(np.int64).tolist())
         image.putpixel((x,y), color)
 
 image.save(output_file)
